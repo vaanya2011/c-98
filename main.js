e2679dc63c1ee5fd8e7a19dd8073abb2148bb47c
vaanya2011 <100355303+vaanya2011@users.noreply.github.com>
2023-02-24 17:37:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var SpeechRecognition= window.webkitSpeechRecognition;
+var recognition= new SpeechRecognition();
+ function start(){
+    document.getElementById("textbox").innerHTML="";
+    recognition.start();
+ }
+ recognition.onresult=function(event){
+    console.log(event);
+
+    var Content = event.results[0][0].trascript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML=Content;
+ }
